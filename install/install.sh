24bd507b2e130425214542a559ce452afc7289b6
Mark Rawling <msr2406@gmail.com>
2020-12-02 13:21:33 +0000

    More install.sh

--- install/install.sh
+++ install/install.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
+#
+# PiScreenClient Installer
+# Author: mark.rawling@piscreen.com
+#License: GNU General Public License v3.0
+#
+
+set -e
+set -E
+set -u
 
 #Default settings
 repo="msrgit/PiScreenClient"
+branch="master"
+default_yes=0
+upgrade=0
 
 #Get the latest version
 readonly PISCREEN_VERSION=$(curl -s "https://api.github.com/repos/$repo/releases/latest" | grep -Po '"tag_name": "\K.*?(?=")' )
@@ -14,6 +26,32 @@ readonly ANSI_RASPBERRY="\033[0;35m"
 readonly ANSI_ERROR="\033[1;37;41m"
 readonly ANSI_RESET="\033[m"
 
+#Parse command line
+while :; do
+    case $1 in
+        -y|--yes|--apt-yes)
+            default_yes=1
+            apt_option="-y"
+            ;;
+        -u|--upgrade)
+            upgrade=1
+            ;;
+        -v|--version)
+            printf "PiScreen Client v${PISCREEN_VERSION} - PiScreen Digital Signage"
+            exit 1
+            ;;
+        -*|--*)
+            echo "Option unknown: $1"
+            exit 1
+            ;;
+        *)
+            break
+            ;;
+    esac
+    shift
+done
+
+UPDATE_URL="https:/raw.githubusercontent.com/$repo/$branch/"
 
 function _display_welcome() {
 echo -e "${ANSI_RASPBERRY}\n"
@@ -28,6 +66,9 @@ echo -e
 
 echo -e "${ANSI_RESET}\n"
 }
+
+
+
 
 function _install_psclient() {
     _display_welcome;
